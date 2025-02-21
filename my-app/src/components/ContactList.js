2663f400c48c8c70e8825de806715e75f5357bc3
MarcoDava <marco.a.dava@gmail.com>
2025-02-21 18:22:57 -0500

    fixed some bugs and starting on remove contact feature

--- my-app/src/components/ContactList.js
+++ my-app/src/components/ContactList.js
@@ -4,23 +4,16 @@ import ContactCard from './ContactCard';
 const ContactList= (props) => {
     console.log(props);
 
+    const deleteContactHandler=(id)=>{
+        props.getContactId(id);
+    };
     const renderContactList = props.contacts.map((contact) => {
         return(
-             <div className='item'>
-                <div className='contact'>
-                    <div className="header">
-                        {contact.name}
-                    </div>
-                    <div>{contact.email}</div>
-                </div>
-                <i className="trash alternate outline icon"></i>
-             </div>
+            <ContactCard contact={contact} clickHandler={deleteContactHandler} key={contact.id}></ContactCard>
         );
     })
 
-    return(
-        <ContactCard contact={contact}></ContactCard>
-    );
+    return <div className="ui celled list">{renderContactList}</div>;
 }
 
 export default ContactList;
